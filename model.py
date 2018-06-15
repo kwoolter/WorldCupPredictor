f0218652dfef5667d30a21aadb468f54b425443d
Keith Woolterton <kwoolter@users.noreply.github.com>
2018-06-15 16:20:17 +0800

    Create model.py

---
+++ model.py
@@ -0,0 +1,80 @@
+import datetime
+import csv
+
+class Team:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+class Fixture:
+    def __init__(self, team_a : Team, team_b : Team, when : datetime, group : str = {"X"}, score : str = None):
+        self.team_a = team_a
+        self.team_b = team_b
+        self.when = when
+        self.group = group
+        self.score = Score(score)
+
+    def __str__(self):
+        return "Group {4}: {0} {3} {1} [{2}]".format(self.team_a,
+                                                     self.team_b,
+                                                     self.when,
+                                                     self.score,
+                                                     self.group)
+
+class Score:
+    def __init__(self, score : str =  None):
+        if score is not None:
+            a,b = score.split(":")
+            self.score_a = int(a)
+            self.score_b = int(b)
+        else:
+            self.score_a = "-"
+            self.score_b = "-"
+
+    def __str__(self):
+        return "{0}:{1}".format(self.score_a, self.score_b)
+
+    def compare(self, other_score):
+        return
+
+class Player:
+    def __init__(self, name : str):
+        self.name = name
+
+class FixtureFactory:
+    def __init__(self):
+        self.fixtures = []
+
+    def load(self):
+        print("\nLoading fixtures...")
+
+        # Attempt to open the file
+        with open(".\\data\\fixtures.csv", 'r') as object_file:
+
+            # Load all rows in as a dictionary
+            reader = csv.DictReader(object_file)
+
+            # Get the list of column headers
+            header = reader.fieldnames
+
+            # For each row in the file....
+            for row in reader:
+                group = row.get("Group")
+                team_a = row.get("TeamA")
+                team_b = row.get("TeamB")
+                when = row.get("When")
+                score = row.get("Score")
+
+                self.fixtures.append(Fixture(team_a, team_b, when, group, score))
+
+
+            # Close the file
+            object_file.close()
+
+    def print(self):
+
+        for fixture in self.fixtures:
+            print(fixture)
+
