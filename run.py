c56522524ad8ff002997034cec7cd737ae9e83bb
kwoolter <keith.woolterton@gmail.com>
2019-05-04 15:08:09 +0800

    tidy up

--- run.py
+++ run.py
@@ -1,16 +1,9 @@
-import model
 import cli
 
 def main():
 
-    # fixtures = model.FixtureFactory()
-    # fixtures.load()
-    # fixtures.print()
-
     c = cli.WCCLI()
     c.cmdloop()
-
-
 
 if __name__ == "__main__":
     main()
